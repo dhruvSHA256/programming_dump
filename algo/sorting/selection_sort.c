a984b010712e9b175e1d29d051932bfed87b1fcb
DHruv <dhruv.sha256@gmail.com>
2020-09-22 18:21:20 +0530

    added doubly linked list

--- algo/sorting/selection_sort.c
+++ algo/sorting/selection_sort.c
@@ -2,40 +2,46 @@
 
 #define MAX 10
 
-void insertion_sort(int *array, int n);
+void selection_sort(int *array, int n);
 
-int main() {
-  int i, n;
-  int array[MAX];
+int main()
+{
+    int i, n;
+    int array[MAX];
 
-  printf("\nEnter Size of Array : ");
+    printf("\nEnter Size of Array : ");
 
-  scanf("%d", &n);
+    scanf("%d", &n);
 
-  printf("\nEnter Elements of Array : ");
-  for (i = 0; i < n; i++) {
-    scanf("%d", &array[i]);
-  }
+    printf("\nEnter Elements of Array : ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
 
-  insertion_sort(array, n);
+    selection_sort(array, n);
 
-  printf("\nSorted Array : ");
+    printf("\nSorted Array : ");
 
-  for (i = 0; i < n; i++) {
-    printf("%d ", array[i]);
-  }
-  return 0;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    return 0;
 }
 
-void insertion_sort(int *array, int n) {
-  int idx, temp;
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i; j < n; j++) {
-      if (array[i] > array[j])
-        idx = j;
+void selection_sort(int *array, int n)
+{
+    int idx, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (array[i] > array[j])
+                idx = j;
+        }
+        temp = array[i];
+        array[i] = array[idx];
+        array[idx] = temp;
     }
-    temp = array[i];
-    array[i] = array[idx];
-    array[idx] = temp;
-  }
 }
--- data_struct/linked_list/douby_linked_list.c
+++ data_struct/linked_list/douby_linked_list.c
@@ -4,13 +4,24 @@
 struct list {
   int data;
   struct list *next;
+  struct list *prev;
 };
 
-void trav(struct list *topnode) {
+void trav(struct list *topnode, int forward) {
   struct list *currnode = topnode;
-  while (currnode) {
-    printf("%d\n", currnode->data);
-    currnode = currnode->next;
+  if (forward == 1) {
+    while (currnode) {
+      printf("%d\n", currnode->data);
+      currnode = currnode->next;
+    }
+  } else {
+    while (currnode->next) {
+      currnode = currnode->next;
+    }
+    while (currnode) {
+      printf("%d\n", currnode->data);
+      currnode = currnode->prev;
+    }
   }
 }
 
@@ -18,6 +29,7 @@ void insert_end(struct list **topnode, int data) {
   struct list *newnode = (struct list *)malloc(sizeof(struct list));
   newnode->data = data;
   newnode->next = NULL;
+  newnode->prev = NULL;
 
   if (*topnode == NULL) {
     *topnode = newnode;
@@ -28,13 +40,16 @@ void insert_end(struct list **topnode, int data) {
       currnode = currnode->next;
     }
     currnode->next = newnode;
+    newnode->prev = currnode;
   }
 }
 
 void insert_beg(struct list **topnode, int data) {
   struct list *newnode = (struct list *)malloc(sizeof(struct list));
   newnode->data = data;
+  newnode->prev = NULL;
   newnode->next = *topnode;
+  (*topnode)->prev = newnode;
   *topnode = newnode;
 }
 
@@ -43,21 +58,22 @@ void insert_pos(struct list *topnode, int data, int pos) {
   struct list *newnode = (struct list *)malloc(sizeof(struct list));
   newnode->data = data;
   struct list *currnode = topnode;
-  for (i=0; i<pos-2; i++) {
-   if(currnode->next){
-        currnode=currnode->next;
-   }
-   else {
-   printf("Linkedlist smaller than given position");
-   return;
-   }
+  for (i = 0; i < pos - 2; i++) {
+    if (currnode->next) {
+      currnode = currnode->next;
+    } else {
+      printf("Linkedlist smaller than given position");
+      return;
+    }
   }
-      newnode->next=currnode->next;
-      currnode->next=newnode;
+  newnode->next = currnode->next;
+  newnode->prev = currnode;
+  currnode->next->prev = newnode;
+  currnode->next = newnode;
 }
 
-void del_end(struct list *topnode) {
-  struct list *currnode = topnode;
+void del_end(struct list **topnode) {
+  struct list *currnode = *topnode;
   while (currnode->next->next) {
     currnode = currnode->next;
   }
@@ -68,8 +84,10 @@ void del_end(struct list *topnode) {
 void del_beg(struct list **topnode) {
   struct list *currnode = *topnode;
   *topnode = currnode->next;
+  (*topnode)->prev = NULL;
   free(currnode);
 }
+
 void del_pos(struct list *topnode, int pos) {
   int i;
   struct list *currnode = topnode;
@@ -83,13 +101,13 @@ void del_pos(struct list *topnode, int pos) {
    }
   }
   struct list *tempnode = currnode->next;
+  currnode->next->next->prev = currnode;
   currnode->next=currnode->next->next;
   free(tempnode);
 }
 
 void freeList(struct list *topnode) {
   struct list *tmp;
-
   while (topnode != NULL) {
     tmp = topnode;
     topnode = topnode->next;
@@ -98,6 +116,7 @@ void freeList(struct list *topnode) {
 }
 
 int main() {
+
   struct list *topnode = NULL;
 
   insert_end(&topnode, 98);
@@ -109,24 +128,27 @@ int main() {
   insert_beg(&topnode, 103);
   insert_beg(&topnode, 104);
 
-  printf("\nTraversal : \n\n");
-  trav(topnode);
+  printf("\nTraversal Forward : \n\n");
+  trav(topnode, 1);
 
-  del_end(topnode);
+  printf("\nTraversal Backward : \n\n");
+  trav(topnode, 0);
+
+  del_end(&topnode);
   printf("\nDel_end : \n\n");
-  trav(topnode);
+  trav(topnode, 1);
 
   del_beg(&topnode);
   printf("\nDel_beg : \n\n");
-  trav(topnode);
+  trav(topnode, 1);
 
   insert_pos(topnode, 9999, 3);
   printf("\nInsertion at pos 3 :\n\n");
-  trav(topnode);
+  trav(topnode, 1);
 
   del_pos(topnode, 3);
   printf("\nDeletion at pos 3 :\n\n");
-  trav(topnode);
+  trav(topnode, 1);
 
   freeList(topnode);
 
