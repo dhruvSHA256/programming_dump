9d5ca319aa6e8a512b67e2bae4ea4c0b0ba7418e
aman-atg <amanansari.a2@gmail.com>
2020-10-02 14:18:57 +0530

    add gcd program

---
+++ common_problems/gcd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int n1, int n2) {
+  return (n2 != 0) ? gcd(n2, n1 % n2) : n1;
+}
+
+int main() {
+  int n1, n2;
+
+  cout << "Enter two positive integers: ";
+  cin >> n1 >> n2;
+
+  cout << "G.C.D of " << n1 << " & " << n2 << " is: " << gcd(n1, n2);
+
+  return 0;
+}
