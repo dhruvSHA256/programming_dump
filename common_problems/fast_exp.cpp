c0ed3bbb50da3142b4408fd911555abc49f7509c
Planet Plut0 <pluto@pmail.pom>
2021-10-30 11:14:52 +0530

    updated fast_expo

--- common_problems/fast_exp.cpp
+++ common_problems/fast_exp.cpp
@@ -8,19 +8,29 @@ using namespace std;
 long long fastexpo(int n, int m);
 
 int main() {
-    int a = 48;
-    cout << fastexpo(a, 9);
-    return 0;
+  int a = 48;
+  cout << fastexpo(a, 9);
+  return 0;
 }
 
 long long fastexpo(int n, int m) {
-    if (m == 0) {
-        return 1;
-    }
-    long long r = fastexpo(n, m / 2);
-    if (m % 2 == 0) {
-        return r * r;
-    } else {
-        return r * r * n;
-    }
+  if (m == 0) {
+    return 1;
+  }
+  long long r = fastexpo(n, m / 2);
+  if (m % 2 == 0) {
+    return r * r;
+  } else {
+    return r * r * n;
+  }
+}
+
+ll fastexpo(int n, int m) {
+  ll res = 1;
+  if (m & 1)
+    res *= n;
+  FOR(m / 2) { n *= n; }
+  res = res * n;
+
+  return res;
 }
