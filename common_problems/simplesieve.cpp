d6bd922aeada3b03d5c617f86fdf76730444653d
Planet Plut0 <pluto@pmail.pom>
2021-10-30 11:20:50 +0530

    updated simplesieve

--- common_problems/simplesieve.cpp
+++ common_problems/simplesieve.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
-
+#include <cstring>
+#include <ctype.h>
 #include <iostream>
+#include <vector>
 
 #define LOG(x) cout << #x << "=" << x << endl;
 #define endl '\n'
@@ -10,27 +11,27 @@ using namespace std;
 void simpleSieve(int n, vector<int> &primes);
 
 int main() {
-    int n;
-    vector<int> primes;
-    cout << "Enter range : ";
-    cin >> n;
+  int n;
+  vector<int> primes;
+  cout << "Enter range : ";
+  cin >> n;
 
-    simpleSieve(n, primes);
-    for (auto p : primes) {
-        cout << p << " ";
-    }
-    return 0;
+  simpleSieve(n, primes);
+  for (auto p : primes) {
+    cout << p << " ";
+  }
+  return 0;
 }
-
 void simpleSieve(int n, vector<int> &primes) {
-    bool mark[n];
-    memset(mark, true, sizeof(mark));
-    for (int p = 2; p * p < n; p++) {
-        if (mark[p] == true) {
-            for (int i = p * p; i < n; i += p) mark[i] = false;
-        }
+  bool mark[n];
+  memset(mark, true, sizeof(mark));
+  for (int p = 2; p * p < n; p++) {
+    if (mark[p] == true) {
+      for (int i = p * p; i < n; i += p)
+        mark[i] = false;
     }
-
-    for (int p = 2; p < n; p++)
-        if (mark[p] == true) primes.push_back(p);
+  }
+  for (int p = 2; p < n; p++)
+    if (mark[p] == true)
+      primes.push_back(p);
 }
